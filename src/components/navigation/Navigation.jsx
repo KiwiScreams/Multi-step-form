e13ffeb0dd48d1d22feff852feeea7a33361d00d
KiwiScreams <screamskiwi3@gmail.com>
2024-07-31 18:57:07 +0400

    navigation style

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -6,28 +6,39 @@ function Navigation() {
             <header>
                 <nav>
                     <ul>
-                        <NavLink className="nav-li" to="personal-info">
+                        <NavLink
+                            to="personal-info"
+                            className={({ isActive }) => `nav-li flex ${isActive ? "active" : ""}`}
+                        >
                             <div className="num">1</div>
                             <div className="nav-text">
                                 <span>STEP 1</span>
                                 <h6>YOUR INFO</h6>
                             </div>
                         </NavLink>
-                        <NavLink className="nav-li" to="select-plan">
+                        <NavLink to="select-plan"
+                            className={({ isActive }) => `nav-li flex ${isActive ? "active" : ""}`}
+                        >
                             <div className="num">2</div>
                             <div className="nav-text">
                                 <span>STEP 2</span>
                                 <h6>SELECT PLAN</h6>
                             </div>
                         </NavLink>
-                        <NavLink className="nav-li" to="select-plan">
+                        <NavLink
+                            to="add-ons"
+                            className={({ isActive }) => `nav-li flex ${isActive ? "active" : ""}`}
+                        >
                             <div className="num">3</div>
                             <div className="nav-text">
                                 <span>STEP 3</span>
                                 <h6>ADD-ONS</h6>
                             </div>
                         </NavLink>
-                        <NavLink className="nav-li" to="select-plan">
+                        <NavLink
+                            to="summary"
+                            className={({ isActive }) => `nav-li flex ${isActive ? "active" : ""}`}
+                        >
                             <div className="num">4</div>
                             <div className="nav-text">
                                 <span>STEP 4</span>
