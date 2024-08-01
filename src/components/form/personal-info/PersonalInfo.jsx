f05fd103253b27c7c9f5a64d040e6d845257d68a
KiwiScreams <screamskiwi3@gmail.com>
2024-08-01 16:08:04 +0400

    error message

--- src/components/form/personal-info/PersonalInfo.jsx
+++ src/components/form/personal-info/PersonalInfo.jsx
@@ -15,15 +15,21 @@ function PersonalInfo() {
                 <h1>Personal info</h1>
                 <p>Please provide your name, email address, and phone number.</p>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <label htmlFor="username">Name</label>
-                    <input type="text" name="username" id="username" placeholder="e.g. Stephen King" {...register("username")} />
-                    {errors.username && errors.username.message}
-                    <label htmlFor="username">Email Address</label>
-                    <input type="email" name="email" id="email" placeholder="e.g. stephenking@lorem.com" {...register("email")} />
-                    {errors.email && errors.email.message}
-                    <label htmlFor="username">Phone Number</label>
-                    <input type="number" name="phone" id="phone" placeholder="e.g. +1 234 567 890" {...register("phone")} />
-                    {errors.phone && errors.phone.message}
+                    <div className="input-box">
+                        <label htmlFor="username">Name</label>
+                        <input type="text" name="username" id="username" placeholder="e.g. Stephen King" {...register("username")} />
+                        {errors.username && <div className="error-message">{errors.username.message}</div>}
+                    </div>
+                    <div className="input-box">
+                        <label htmlFor="username">Email Address</label>
+                        <input type="email" name="email" id="email" placeholder="e.g. stephenking@lorem.com" {...register("email")} />
+                        {errors.email && <div className="error-message">{errors.email.message}</div>}
+                    </div>
+                    <div className="input-box">
+                        <label htmlFor="username">Phone Number</label>
+                        <input type="number" name="phone" id="phone" placeholder="e.g. +1 234 567 890" {...register("phone")} />
+                        {errors.phone && <div className="error-message">{errors.phone.message}</div>}
+                    </div>
                     <Button text="Next Step"></Button>
                 </form>
             </section>
--- src/validation-schema.js
+++ src/validation-schema.js
@@ -1,7 +1,7 @@
 import * as yup from "yup"
 const validationSchema = yup.object().shape({
-    username: yup.string().min(2).max(22).required(),
-    email: yup.string().email().required(),
-    phone: yup.number().min(9).max(12).required(),
+    username: yup.string().min(2).max(22).required("This field is required"),
+    email: yup.string().email().required("This field is required"),
+    phone: yup.number().min(9).max(12).required("This field is required"),
 });
 export default validationSchema;
